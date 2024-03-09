d7b322e8409b615163d00c9c53fd3ad87540046a
Gleb Trusov <gleb.trusow@yandex.ru>
2024-03-09 22:33:30 +0300

    Подключил БД PostgreSQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version = "0.48.0"
 
 plugins {
     kotlin("jvm") version "1.9.22"
@@ -28,6 +29,13 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-server-cio-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
+    implementation("org.postgresql:postgresql:42.7.0")
+
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- src/main/kotlin/ru/mobdev/Application.kt
+++ src/main/kotlin/ru/mobdev/Application.kt
@@ -3,9 +3,16 @@ package ru.mobdev
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import org.jetbrains.exposed.sql.Database
 import ru.mobdev.plugins.*
 
 fun main() {
+    Database.connect(
+        url = "jdbc:postgresql://localhost:5432/mobile_developer_course",
+        driver = "org.postgresql.Driver",
+        password = "postgres"
+    )
+
     embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
